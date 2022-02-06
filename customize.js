933e2f20ae3808fe00e1b6aa7d5753130f73eccc
Baptiste <baptiste@roullin.net>
2022-02-06 19:14:20 +0100

    update

---
+++ customize.js
@@ -0,0 +1,12 @@
+
+Reveal.configure({
+	progress: false,
+	menu: {
+		openButton: false,
+		markers: false,
+		hideMissingTitles: true,
+	}
+})
+Reveal.registerPlugin(RevealMenu)
+Reveal.registerPlugin(RevealSearch)
+
